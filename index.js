a86f66c91a0115f39113131c024aa656738f5996
Mehwish Kapadia <mehwishkap@gmail.com>
2018-01-21 18:50:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ function inc(n){
 number +=1
 return (number)
 }
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
